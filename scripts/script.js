8ad1f8054015337d8cdbe96b6aeaa31c36d70112
NM <38670052+nmelgar@users.noreply.github.com>
2021-07-20 22:32:16 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+
+const hamburguer = document.getElementById('hamburguer');
+const navUL = document.getElementById('list-of-item');
+
+hamburguer.addEventListener('click', ()  =>{
+  navUL.classList.toggle('active');
+
+});
+
+// let btn = document.getElementById('hamburguer');
+// let div = document.getElementById('list-of-item');
+//
+// btn.addEventListener('click', () =>{
+//   if(div.style.display === 'none'){
+//     div.style.display = 'grid';
+//   }else{
+//     div.style.display = 'none';
+//   }
+// })
